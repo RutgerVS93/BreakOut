d8477ef40b6bfcf3beab8745e193e6da6417d472
RutgerVS93 <Rutger.van.Schoonderwalt@gmail.com>
2018-06-07 11:26:27 +0200

    Cloned folder

---
+++ Assets/Scripts/PaddleBeamScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBeamScript : MonoBehaviour {
+
+    public GameObject parent;
+
+    public GameObject beamLine;
+    public GameObject beamSide;
+
+    private GameObject beamSideLeft;
+    private GameObject beamSideRight;
+    private GameObject beam;
+
+    private LineRenderer line;
+
+    public float textureScrollSpeed = 8f; //How fast the texture scrolls along the beam
+    public float textureLengthScale = 3; //Length of the beam texture
+
+    void Start ()
+    {
+        beamSideLeft = Instantiate(beamSide, new Vector3(-5, -18, 0), Quaternion.identity) as GameObject;
+        beamSideLeft.transform.parent = parent.transform;
+        beamSideRight = Instantiate(beamSide, new Vector3(5, -18, 0), Quaternion.identity) as GameObject;
+        beamSideRight.transform.parent = parent.transform;
+        beam = Instantiate(beamLine, Vector3.zero, Quaternion.identity) as GameObject;
+        beam.transform.parent = parent.transform;
+        line = beam.GetComponent<LineRenderer>();
+	}
+
+    void ShootBeamInDir(Vector3 start, Vector3 end)
+    {
+        line.positionCount = 2;
+
+        line.SetPosition(0, start);
+        beamSideLeft.transform.position = start;
+
+        line.SetPosition(1, end);
+        beamSideRight.transform.position = end;
+
+        float distance = Vector2.Distance(start, end);
+        line.sharedMaterial.mainTextureScale = new Vector2(distance / textureLengthScale, 1);
+        line.sharedMaterial.mainTextureOffset -= new Vector2(Time.deltaTime * textureScrollSpeed, 0);
+    }
+
+    void Update ()
+    {
+        ShootBeamInDir(beamSideLeft.transform.position, beamSideRight.transform.position);
+    }
+}
---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    public float speed;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+        Movement();
+	}
+
+    void Movement()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        if (h != 0)
+        {
+            rb.velocity = new Vector2(h * speed, 0);
+        }
+    }
+}
